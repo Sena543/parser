cef0d7813f42d364256ab9bd32ed917208687f0c
senanu <senanuazanu@gmail.com>
2024-06-17 22:01:01 +0000

    ...

--- src/abstractSyntaxTree.go
+++ src/abstractSyntaxTree.go
@@ -1,7 +1,21 @@
 package src
 
-type Node struct {
+type Node interface {
+	TokenLexeme() string
+}
+
+type Key interface {
+	Node
+	keyNode()
+}
+
+type Value interface {
+	Node
+	valueNode()
+}
+
+/* type Node struct {
 	operator Token
 	left     *Node
 	right    *Node
-}
+} */
