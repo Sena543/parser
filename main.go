285b9d9f6b9c9c2d5891becd4c69e20403ae2fe2
Sena <senanuazanu@gmail.com>
2024-06-24 15:33:33 +0000

    add: ability to read from both stdin and file path is provided

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"parser/src"
@@ -11,19 +12,38 @@ func main() {
 	readFile()
 }
 
-func readFile() {
-	/* jsonPath := "./src/tests/step2/valid.json" */
-	/* jsonPath := "./src/tests/step2/invalid.json" */
-	jsonPath := "./src/tests/step3/invalid.json"
-	/* jsonPath := "./src/tests/step3/valid.json" */
-	/* jsonPath := "./src/tests/step3/invalid.json" */
-	fileBytes, error := os.ReadFile(jsonPath)
-	if error != nil {
-		fmt.Println("File reading error: ", error)
+func checkError(err error, msg string) {
+	if err != nil {
+		fmt.Println(fmt.Errorf(msg, err))
 	}
-	byteData := src.BeginScan(fileBytes)
+}
+
+func readFile() {
+	stdIn, err := os.Stdin.Stat()
+	checkError(err, "Error checking stdin")
+
+	/* jsonPath := "./src/test_files/step2/valid.json" */
+	/* jsonPath := "./src/test_files/step2/invalid.json" */
+	/* jsonPath := "./src/tests_files/step3/valid.json" */
+	/* jsonPath := "./src/tests_files/step3/ivalid.json" */
+	/* jsonPath := "./src/tests_files/step3/valid.json" */
+	/* jsonPath := "./src/tests_files/step3/invalid.json" */
+	/* fileBytes, err := os.ReadFile(jsonPath) */
+	/* checkError(err, "error reading file: ") */
+	var fileDataPointer []byte
+	if stdIn.Mode()&os.ModeCharDevice == 0 { //check if data is comming from stdin
+		fileDataPointer, _ = io.ReadAll(os.Stdin)
+	} else {
+		filePath := os.Args[len(os.Args)-1]
+		if _, err := os.Stat(filePath); err != nil { //check if path exists
+			checkError(err, "File path error: ")
+		}
+		fileDataPointer, _ = os.ReadFile(filePath)
+	}
+
+	byteData := src.BeginScan(fileDataPointer)
 	// byteData.ScanTokens()
 	/* byteData.ScannerLoop() */
 	parserInit := src.New(byteData)
-	parserInit.ParserLoop()
+	parserInit.ParserLoop(os.Stdout)
 }
