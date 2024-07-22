b2a0e3e7a5885e55d2b95d36e3a61b004369d44a
senanu <senanuazanu@gmail.com>
2024-07-22 19:18:16 +0000

    better error handling with error being propagated

--- src/parser.go
+++ src/parser.go
@@ -1,120 +1,170 @@
 package src
 
 import (
+	"errors"
 	"fmt"
 	"io"
-	"os"
 )
 
 type Parser struct {
-	lexer                   *Lexer
-	currentToken, nextToken Token
-	stdout                  io.Writer
+	lexer        *Lexer
+	currentToken Token
+	nextToken    *Token
+	message      string
+	parseErr     error
 	//nextToken is my lookahead
 }
 
 func New(lexer *Lexer) *Parser {
 	parser := &Parser{lexer: lexer}
-	parser.stdout = os.Stdout //print to stdout by default
-	parser.getNextToken()     //called twice to populate the two token fields in Parser struct
-	parser.getNextToken()     //cos on first call p.nextToken is nil, second call yeilds the scanned value
+	parser.getNextToken() //called twice to populate the two token fields in Parser struct
+	parser.getNextToken() //cos on first call p.nextToken is nil, second call yeilds the scanned value
 	return parser
 }
 
-func (p *Parser) getNextToken() {
-	p.currentToken = p.nextToken
-	p.nextToken = p.lexer.ScanTokens()
+func (p *Parser) getNextToken() error {
+	if p.nextToken == nil {
+		p.currentToken = Token{}
+	} else {
+		p.currentToken = *p.nextToken
+	}
+	token := p.lexer.ScanTokens()
+	p.nextToken = &token
+
+	if p.nextToken == nil {
+		return errors.New("Error: nil next token")
+	}
+
+	return nil
 }
 
-func (p *Parser) ParserLoop(writer io.Writer) {
+func (p *Parser) ParserLoop(writer io.Writer) (string, error) {
 	for p.currentToken.TokenType != EOF {
+		/* for p.currentToken.TokenType != EOF && p.message == "" { */
 		if p.currentToken.TokenType == LEFT_BRACE {
-			p.ParseObjects()
+			if err := p.ParseObjects(); err != nil {
+				return "invalid", err
+			}
+
 		} else if p.currentToken.TokenType == LEFT_PAREN {
-			p.ParseArray()
+			if err := p.ParseArray(); err != nil {
+				return "invalid", err
+			}
 		} else {
-			p.ParseValue()
+			if err := p.ParseValue(); err != nil {
+				return "invalid", err
+			}
 		}
 		p.getNextToken()
 	}
 
-	if p.currentTokenIs(EOF) {
-		fmt.Fprintln(writer, "valid")
-	}
+	return "valid", nil
 }
 
-func (p *Parser) ParseObjects() {
+func (p *Parser) ParseObjects() error {
 
 	p.match(LEFT_BRACE)
 	for !p.currentTokenIs(RIGHT_BRACE) {
-		p.match(KEY)
-		p.match(COLON)
+		if err := p.match(KEY); err != nil {
+			return err
+		}
+
+		if err := p.match(COLON); err != nil {
+			return err
+		}
+
 		if p.nextTokenIs(COMMA) {
-			p.ParseValue()
+			if err := p.ParseValue(); err != nil {
+				return err
+			}
+
 			if p.nextTokenIs(RIGHT_BRACE) {
 				p.match(COMMA)
-				/* panic("trailing comma") */
-				p.parserError("trailing comma")
+				return errors.New("trailing comma")
 			} else {
-				p.match(COMMA)
+				if err := p.match(COMMA); err != nil {
+					return err
+				}
 			}
 		} else {
-			p.ParseValue()
+			if err := p.ParseValue(); err != nil {
+				return err
+			}
 		}
 	}
 
-	p.match(RIGHT_BRACE)
-	if p.nextTokenIs(KEY) {
-		p.match(COMMA)
+	/* p.match(RIGHT_BRACE) */
+	if err := p.match(RIGHT_BRACE); err != nil {
+		return err
 	}
+
+	if p.nextTokenIs(KEY) {
+		if err := p.match(COMMA); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func (p *Parser) ParseValue() {
+func (p *Parser) ParseValue() error {
 
+	/* fmt.Println("current token:", p.currentToken, "next token: ", *p.nextToken) */
 	switch p.currentToken.TokenType {
 	case STRING_VALUE:
-		p.match(STRING_VALUE)
+		return p.match(STRING_VALUE)
 	case LEFT_BRACE:
-		p.ParseObjects()
+		return p.ParseObjects()
 	case LEFT_PAREN:
-		p.ParseArray()
+		return p.ParseArray()
 	case TRUE:
-		p.match(TRUE)
+		return p.match(TRUE)
 	case FALSE:
-		p.match(FALSE)
+		return p.match(FALSE)
 	case NUMBER:
-		p.match(NUMBER)
+		return p.match(NUMBER)
 	case NULL:
-		p.match(NULL)
+		return p.match(NULL)
+	case ILLEGAL:
+		return errors.New("illegal token found")
 	default:
 		p.parserError("default: illegal value")
-		p.IllegalValue(p.currentToken)
+		/* p.getNextToken() */
+		/* fmt.Println(p.message, "messagekdfkajd") */
 	}
 
-}
-
-func (p *Parser) IllegalValue(expectedToken Token) {
-
-	msg := fmt.Sprintf("Unexpected value %s", expectedToken.Lexeme)
-	fmt.Println(p.stdout, msg)
-	panic(msg)
+	return nil
 }
 
 func (p *Parser) parserError(errMsg string) {
-	fmt.Println(p.stdout, errMsg)
-	os.Exit(1)
+	/* func (p *Parser) parserError(errMsg string, expectedToken string) error { */
+	/* if err := p.match(expectedToken); err != nil {
+		return err
+	} */
+	p.message = errMsg
+	fmt.Println(errMsg, p.currentToken)
+	/* return nil */
 }
 
-func (p *Parser) ParseArray() {
+func (p *Parser) ParseArray() error {
 
-	p.match(LEFT_PAREN)
+	if err := p.match(LEFT_PAREN); err != nil {
+		return err
+	}
 	for !p.currentTokenIs(RIGHT_PAREN) {
-		p.ParseValue()
+		if err := p.ParseValue(); err != nil {
+			return err
+		}
 		if p.nextTokenIs(COMMA) {
-			p.match(COMMA)
+			if err := p.match(COMMA); err != nil {
+				return err
+			}
 		}
 	}
-	p.match(RIGHT_PAREN)
+
+	if err := p.match(RIGHT_PAREN); err != nil {
+		return err
+	}
+	return nil
 }
 
 // checks the what we expect the nextToken to be
@@ -126,15 +176,19 @@ func (p *Parser) expect(tk string) bool {
 	return true
 }
 
-func (p *Parser) match(expectedToken string) {
-	/* fmt.Println(expectedToken, p.currentToken.Lexeme, "next token:", p.nextToken.Lexeme, p.nextToken.TokenType) */
+func (p *Parser) match(expectedToken string) error {
+	/* fmt.Println("expectedToken: ", expectedToken, p.currentToken.Lexeme, "<-->next token:", p.nextToken.Lexeme, p.nextToken.TokenType) */
+	fmt.Println("expectedToken: ", expectedToken, "currentToken: ", p.currentToken, "nextToken: ", p.nextToken)
 	if p.currentToken.TokenType == expectedToken {
-		p.getNextToken()
-		return
+		err := p.getNextToken()
+		if err != nil {
+			return err
+		}
+		return nil
 	}
 	msg := fmt.Sprintf("Expected %s got %s", expectedToken, p.currentToken.TokenType)
-	p.parserError(msg)
-	/* panic(msg) */
+	/* p.parserError(msg) */
+	return errors.New(msg)
 }
 
 func (p *Parser) currentTokenIs(tk string) bool {
