e36bdda42b9d1e3c1ffa2cd163251402a8a1b06d
senanu <senanuazanu@gmail.com>
2024-07-22 19:18:44 +0000

    tests rewrites

--- src/parser_test.go
+++ src/parser_test.go
@@ -2,6 +2,7 @@ package src
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"os"
 	"testing"
@@ -24,8 +25,7 @@ func TestParser(t *testing.T) {
 			path string
 			want string
 		}{
-			{path: "./tests_files/step1/valid.json", want: "valid\n"},
-			{path: "./tests_files/step1/invalid.json", want: "Error: file empty"},
+			{path: "./tests_files/step1/valid.json", want: "valid"},
 			/* {path: "./tests_files/step1/invalid.json", want: "Error: file empty"}, */
 		}
 		for _, value := range step1 {
@@ -34,30 +34,96 @@ func TestParser(t *testing.T) {
 				buffer := bytes.Buffer{}
 
 				parse := readFile(t, value.path)
-				parse.ParserLoop(&buffer)
-				got := buffer.String()
+				got, _ := parse.ParserLoop(&buffer)
+				/* got := buffer.String() */
 				want := value.want
 
 				if got != want {
 					t.Errorf("got %q want %q \n", got, want)
 				}
 				buffer.Reset()
-
 			})
+		}
+	})
+	t.Run("step2: ", func(t *testing.T) {
+		step2 := []struct {
+			path    string
+			want    string
+			message error
+		}{
+			{path: "./tests_files/step2/valid.json", want: "valid", message: nil},
+			{path: "./tests_files/step2/invalid.json", want: "invalid", message: errors.New("trailing comma")},
+			{path: "./tests_files/step2/valid2.json", want: "valid", message: nil},
+			{path: "./tests_files/step2/invalid2.json", want: "invalid", message: errors.New("Expected KEY got ILLEGAL")},
+		}
+		for _, value := range step2 {
+			t.Run(value.path, func(t *testing.T) {
 
+				buffer := bytes.Buffer{}
+
+				parse := readFile(t, value.path)
+				/* got, errMsg := parse.ParserLoop(&buffer) */
+				got, _ := parse.ParserLoop(&buffer)
+				want := value.want
+
+				if got != want {
+					t.Errorf("got %q want %q \n", got, want)
+				}
+				/* 		if value.message != errMsg {
+					t.Errorf("got %q want %q \n", got, want)
+				}
+				*/buffer.Reset()
+			})
+		}
+	})
+	t.Run("step3: ", func(t *testing.T) {
+		step3 := []struct {
+			path    string
+			want    string
+			message error
+		}{
+			{path: "./tests_files/step3/valid.json", want: "valid", message: nil},
+			{path: "./tests_files/step3/invalid.json", want: "invalid", message: errors.New("Value expected")},
+		}
+		for _, value := range step3 {
+			t.Run(value.path, func(t *testing.T) {
+
+				buffer := bytes.Buffer{}
+				parse := readFile(t, value.path)
+				got, el := parse.ParserLoop(&buffer)
+				want := value.want
+
+				fmt.Println("got", el)
+				if got != want {
+					t.Errorf("got %q want %q \n", got, want)
+				}
+			})
 		}
 
 	})
-	/* t.Run("step1: invalid.json test", func(t *testing.T) {
-		 jsonPath := "./src/test_files/step2/invalid.json"
-	}) */
-	/* t.Run("step1: invalid.json test", func(t *testing.T) {
-		 jsonPath := "./src/test_files/step2/invalid.json"
-	}) */
-	/* t.Run("step2: invalid.json test", func(t *testing.T) {
-		 jsonPath := "./src/test_files/step2/invalid.json"
-	}) */
-	/* t.Run("step2: invalid.json test", func(t *testing.T) {
-		 jsonPath := "./src/test_files/step2/invalid.json"
+	/* t.Run("step4: ", func(t *testing.T) {
+		step4 := []struct {
+			path    string
+			want    string
+			message error
+		}{
+			{path: "./tests_files/step4/valid.json", want: "valid", message: nil},
+			{path: "./tests_files/step4/valid2.json", want: "valid", message: nil},
+			{path: "./tests_files/step4/invalid.json", want: "invalid", message: errors.New("trailing comma")},
+		}
+		for _, value := range step4 {
+			t.Run(value.path, func(t *testing.T) {
+
+				buffer := bytes.Buffer{}
+				parse := readFile(t, value.path)
+				got, _ := parse.ParserLoop(&buffer)
+				want := value.want
+
+				if got != want {
+					t.Errorf("got %q want %q \n", got, want)
+				}
+			})
+		}
+
 	}) */
 }
